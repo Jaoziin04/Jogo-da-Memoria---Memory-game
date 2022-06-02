d3024b88e417cc6ab4f8e4d4d3583f4297bee3ab
JOAO VICTOR CRUZ RIBEIRO <u21239@academico.cotuca.unicamp.br>
2022-06-02 09:04:22 -0300

    adicionando jogo da memoria

---
+++ jogo_de_memoria/cc21728rafaelcguerinojogodememoria/cc21728rafaelcguerinojogodememoria/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace cc21728rafaelcguerinojogodememoria
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbNaruto = new System.Windows.Forms.PictureBox();
+            this.pbNarutoPar = new System.Windows.Forms.PictureBox();
+            this.pbSasuke = new System.Windows.Forms.PictureBox();
+            this.pbSsukePar = new System.Windows.Forms.PictureBox();
+            this.pbSakura = new System.Windows.Forms.PictureBox();
+            this.pbSakuraPar = new System.Windows.Forms.PictureBox();
+            this.pbKakashi = new System.Windows.Forms.PictureBox();
+            this.pbKakashiPar = new System.Windows.Forms.PictureBox();
+            this.pbHinata = new System.Windows.Forms.PictureBox();
+            this.pbHinataPar = new System.Windows.Forms.PictureBox();
+            this.pbShikamaru = new System.Windows.Forms.PictureBox();
+            this.pbShikamaruPar = new System.Windows.Forms.PictureBox();
+            this.lbMov = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbNaruto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbNarutoPar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSasuke)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSsukePar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSakura)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSakuraPar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKakashi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKakashiPar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHinata)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHinataPar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbShikamaru)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbShikamaruPar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbNaruto
+            // 
+            this.pbNaruto.BackColor = System.Drawing.Color.Transparent;
+            this.pbNaruto.Image = global::cc21728rafaelcguerinojogodememoria.Properties.Resources.naruto;
+            this.pbNaruto.Location = new System.Drawing.Point(79, 80);
+            this.pbNaruto.Name = "pbNaruto";
+            this.pbNaruto.Size = new System.Drawing.Size(118, 66);
+            this.pbNaruto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbNaruto.TabIndex = 0;
+            this.pbNaruto.TabStop = false;
+            this.pbNaruto.Tag = "0";
+            this.pbNaruto.Click += new System.EventHandler(this.pbNaruto_Click);
+            // 
+            // pbNarutoPar
+            // 
+            this.pbNarutoPar.BackColor = System.Drawing.Color.Transparent;
+            this.pbNarutoPar.Image = global::cc21728rafaelcguerinojogodememoria.Properties.Resources.naruto;
+            this.pbNarutoPar.Location = new System.Drawing.Point(216, 80);
+            this.pbNarutoPar.Name = "pbNarutoPar";
+            this.pbNarutoPar.Size = new System.Drawing.Size(118, 66);
+            this.pbNarutoPar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbNarutoPar.TabIndex = 1;
+            this.pbNarutoPar.TabStop = false;
+            this.pbNarutoPar.Tag = "0";
+            this.pbNarutoPar.Click += new System.EventHandler(this.pbNaruto_Click);
+            // 
+            // pbSasuke
+            // 
+            this.pbSasuke.BackColor = System.Drawing.Color.Transparent;
+            this.pbSasuke.Image = global::cc21728rafaelcguerinojogodememoria.Properties.Resources.sasuke;
+            this.pbSasuke.Location = new System.Drawing.Point(359, 80);
+            this.pbSasuke.Name = "pbSasuke";
+            this.pbSasuke.Size = new System.Drawing.Size(100, 64);
+            this.pbSasuke.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbSasuke.TabIndex = 2;
+            this.pbSasuke.TabStop = false;
+            this.pbSasuke.Tag = "1";
+            this.pbSasuke.Click += new System.EventHandler(this.pbNaruto_Click);
+            // 
+            // pbSsukePar
+            // 
+            this.pbSsukePar.BackColor = System.Drawing.Color.Transparent;
+            this.pbSsukePar.Image = global::cc21728rafaelcguerinojogodememoria.Properties.Resources.sasuke;
+            this.pbSsukePar.Location = new System.Drawing.Point(472, 80);
+            this.pbSsukePar.Name = "pbSsukePar";
+            this.pbSsukePar.Size = new System.Drawing.Size(106, 64);
+            this.pbSsukePar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbSsukePar.TabIndex = 3;
+            this.pbSsukePar.TabStop = false;
+            this.pbSsukePar.Tag = "1";
+            this.pbSsukePar.Click += new System.EventHandler(this.pbNaruto_Click);
+            // 
+            // pbSakura
+            // 
+            this.pbSakura.BackColor = System.Drawing.Color.Transparent;
+            this.pbSakura.Image = global::cc21728rafaelcguerinojogodememoria.Properties.Resources.sakura;
+            this.pbSakura.Location = new System.Drawing.Point(79, 175);
+            this.pbSakura.Name = "pbSakura";
+            this.pbSakura.Size = new System.Drawing.Size(110, 64);
+            this.pbSakura.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbSakura.TabIndex = 4;
+            this.pbSakura.TabStop = false;
+            this.pbSakura.Tag = "2";
+            this.pbSakura.Click += new System.EventHandler(this.pbNaruto_Click);
+            // 
+            // pbSakuraPar
+            // 
+            this.pbSakuraPar.BackColor = System.Drawing.Color.Transparent;
+            this.pbSakuraPar.Image = global::cc21728rafaelcguerinojogodememoria.Properties.Resources.sakura;
+            this.pbSakuraPar.Location = new System.Drawing.Point(216, 175);
+            this.pbSakuraPar.Name = "pbSakuraPar";
+            this.pbSakuraPar.Size = new System.Drawing.Size(103, 64);
+            this.pbSakuraPar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbSakuraPar.TabIndex = 5;
+            this.pbSakuraPar.TabStop = false;
+            this.pbSakuraPar.Tag = "2";
+            this.pbSakuraPar.Click += new System.EventHandler(this.pbNaruto_Click);
+            // 
+            // pbKakashi
+            // 
+            this.pbKakashi.BackColor = System.Drawing.Color.Transparent;
+            this.pbKakashi.Image = global::cc21728rafaelcguerinojogodememoria.Properties.Resources.kakashi;
+            this.pbKakashi.Location = new System.Drawing.Point(472, 175);
+            this.pbKakashi.Name = "pbKakashi";
+            this.pbKakashi.Size = new System.Drawing.Size(100, 62);
+            this.pbKakashi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbKakashi.TabIndex = 6;
+            this.pbKakashi.TabStop = false;
+            this.pbKakashi.Tag = "3";
+            this.pbKakashi.Click += new System.EventHandler(this.pbNaruto_Click);
+            // 
+            // pbKakashiPar
+            // 
+            this.pbKakashiPar.BackColor = System.Drawing.Color.Transparent;
+            this.pbKakashiPar.Image = global::cc21728rafaelcguerinojogodememoria.Properties.Resources.kakashi;
+            this.pbKakashiPar.Location = new System.Drawing.Point(359, 175);
+            this.pbKakashiPar.Name = "pbKakashiPar";
+            this.pbKakashiPar.Size = new System.Drawing.Size(93, 62);
+            this.pbKakashiPar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbKakashiPar.TabIndex = 7;
+            this.pbKakashiPar.TabStop = false;
+            this.pbKakashiPar.Tag = "3";
+            this.pbKakashiPar.Click += new System.EventHandler(this.pbNaruto_Click);
+            // 
+            // pbHinata
+            // 
+            this.pbHinata.BackColor = System.Drawing.Color.Transparent;
+            this.pbHinata.Image = global::cc21728rafaelcguerinojogodememoria.Properties.Resources.hinata;
+            this.pbHinata.Location = new System.Drawing.Point(79, 277);
+            this.pbHinata.Name = "pbHinata";
+            this.pbHinata.Size = new System.Drawing.Size(110, 62);
+            this.pbHinata.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbHinata.TabIndex = 8;
+            this.pbHinata.TabStop = false;
+            this.pbHinata.Tag = "4";
+            this.pbHinata.Click += new System.EventHandler(this.pbNaruto_Click);
+            // 
+            // pbHinataPar
+            // 
+            this.pbHinataPar.BackColor = System.Drawing.Color.Transparent;
+            this.pbHinataPar.Image = global::cc21728rafaelcguerinojogodememoria.Properties.Resources.hinata;
+            this.pbHinataPar.Location = new System.Drawing.Point(216, 277);
+            this.pbHinataPar.Name = "pbHinataPar";
+            this.pbHinataPar.Size = new System.Drawing.Size(103, 62);
+            this.pbHinataPar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbHinataPar.TabIndex = 9;
+            this.pbHinataPar.TabStop = false;
+            this.pbHinataPar.Tag = "4";
+            this.pbHinataPar.Click += new System.EventHandler(this.pbNaruto_Click);
+            // 
+            // pbShikamaru
+            // 
+            this.pbShikamaru.BackColor = System.Drawing.Color.Transparent;
+            this.pbShikamaru.Image = global::cc21728rafaelcguerinojogodememoria.Properties.Resources.shikamaru;
+            this.pbShikamaru.Location = new System.Drawing.Point(359, 277);
+            this.pbShikamaru.Name = "pbShikamaru";
+            this.pbShikamaru.Size = new System.Drawing.Size(100, 62);
+            this.pbShikamaru.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbShikamaru.TabIndex = 10;
+            this.pbShikamaru.TabStop = false;
+            this.pbShikamaru.Tag = "5";
+            this.pbShikamaru.Click += new System.EventHandler(this.pbNaruto_Click);
+            // 
+            // pbShikamaruPar
+            // 
+            this.pbShikamaruPar.BackColor = System.Drawing.Color.Transparent;
+            this.pbShikamaruPar.Image = global::cc21728rafaelcguerinojogodememoria.Properties.Resources.shikamaru;
+            this.pbShikamaruPar.Location = new System.Drawing.Point(472, 277);
+            this.pbShikamaruPar.Name = "pbShikamaruPar";
+            this.pbShikamaruPar.Size = new System.Drawing.Size(100, 62);
+            this.pbShikamaruPar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbShikamaruPar.TabIndex = 11;
+            this.pbShikamaruPar.TabStop = false;
+            this.pbShikamaruPar.Tag = "5";
+            this.pbShikamaruPar.Click += new System.EventHandler(this.pbNaruto_Click);
+            // 
+            // lbMov
+            // 
+            this.lbMov.AutoSize = true;
+            this.lbMov.BackColor = System.Drawing.Color.Transparent;
+            this.lbMov.Location = new System.Drawing.Point(649, 57);
+            this.lbMov.Name = "lbMov";
+            this.lbMov.Size = new System.Drawing.Size(67, 13);
+            this.lbMov.TabIndex = 12;
+            this.lbMov.Text = "Movimentos:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::cc21728rafaelcguerinojogodememoria.Properties.Resources.narutoPaisa;
+            this.ClientSize = new System.Drawing.Size(846, 450);
+            this.Controls.Add(this.lbMov);
+            this.Controls.Add(this.pbShikamaruPar);
+            this.Controls.Add(this.pbShikamaru);
+            this.Controls.Add(this.pbHinataPar);
+            this.Controls.Add(this.pbHinata);
+            this.Controls.Add(this.pbKakashiPar);
+            this.Controls.Add(this.pbKakashi);
+            this.Controls.Add(this.pbSakuraPar);
+            this.Controls.Add(this.pbSakura);
+            this.Controls.Add(this.pbSsukePar);
+            this.Controls.Add(this.pbSasuke);
+            this.Controls.Add(this.pbNarutoPar);
+            this.Controls.Add(this.pbNaruto);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo da Memória Naruto";
+            ((System.ComponentModel.ISupportInitialize)(this.pbNaruto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbNarutoPar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSasuke)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSsukePar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSakura)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSakuraPar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKakashi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKakashiPar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHinata)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHinataPar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbShikamaru)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbShikamaruPar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbNaruto;
+        private System.Windows.Forms.PictureBox pbNarutoPar;
+        private System.Windows.Forms.PictureBox pbSasuke;
+        private System.Windows.Forms.PictureBox pbSsukePar;
+        private System.Windows.Forms.PictureBox pbSakura;
+        private System.Windows.Forms.PictureBox pbSakuraPar;
+        private System.Windows.Forms.PictureBox pbKakashi;
+        private System.Windows.Forms.PictureBox pbKakashiPar;
+        private System.Windows.Forms.PictureBox pbHinata;
+        private System.Windows.Forms.PictureBox pbHinataPar;
+        private System.Windows.Forms.PictureBox pbShikamaru;
+        private System.Windows.Forms.PictureBox pbShikamaruPar;
+        private System.Windows.Forms.Label lbMov;
+    }
+}
+
---
+++ jogo_de_memoria/cc21728rafaelcguerinojogodememoria/cc21728rafaelcguerinojogodememoria/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace cc21728rafaelcguerinojogodememoria
+{
+    public partial class Form1 : Form
+    {
+
+        int mov, click, cartasEncontradas, tag;
+
+        Image[] img = new Image[6];
+
+        List<string> lista = new List<string>();
+
+        int[] guardarTag = new int[2];
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            Começar();
+        }
+
+
+        void Começar()
+        {
+            foreach (PictureBox imagem in Controls.OfType<PictureBox>()) // percorre as pictureBox
+            {
+                tag = int.Parse(String.Format("{0}", imagem.Tag)); // converte a tag da imagem para um inteiro
+                img[tag] = imagem.Image;
+                imagem.Image = Properties.Resources.narutoLogo; // deixa todas as imagem com o verso para cima
+                imagem.Enabled = true;
+            }
+            Embaralhar();
+        }
+
+
+        void Embaralhar()
+        {
+            foreach (PictureBox imagem in Controls.OfType<PictureBox>())
+            {
+                Random aleatorio = new Random();
+
+                int[] x = { 79, 216, 359, 472 }; // posições na horizontal
+                int[] y = { 80, 175, 277 }; // posições na vertical
+
+            Repetir:
+                var xPosicao = x[aleatorio.Next(0, x.Length)]; // pega uma posição aleatória na horizonta
+                var yPosicao = y[aleatorio.Next(0, y.Length)]; // pega uma posição aleatória na vertical
+
+                imagem.Location = new Point(xPosicao, yPosicao); // coloca a imagem nas posições aleatórias
+
+                string verificar = xPosicao.ToString() + yPosicao.ToString();
+
+                if (lista.Contains(verificar)) // verifica se já há uma imagem na posição sorteada
+                {
+                    goto Repetir;
+                }
+                else
+                {
+                    imagem.Location = new Point(xPosicao, yPosicao); // coloca a imagem nas posições aleatórias
+                    lista.Add(verificar); // adiciona as posições a lista   
+                }
+            }
+        }
+        private void pbNaruto_Click(object sender, EventArgs e)
+        {
+            bool acerto = false;
+
+            PictureBox pb = (PictureBox)sender;
+            click++;
+            tag = int.Parse(String.Format("{0}", pb.Tag));
+            pb.Image = img[tag];
+            pb.Refresh();
+
+            if (click == 1)
+            {
+                guardarTag[0] = int.Parse(String.Format("{0}", pb.Tag));
+            }
+            else if (click == 2)
+            {
+                mov++;
+                lbMov.Text = "Movimentos :" + mov.ToString();
+                guardarTag[1] = int.Parse(String.Format("{0}", pb.Tag));
+                acerto = AcertouPares();
+                RevelarCarta(acerto);
+            }
+
+        }
+
+        bool AcertouPares() // este metdo verifica se o jogador acertou os pares das cartas 
+        {
+            click = 0;
+
+            if (guardarTag[0] == guardarTag[1])
+                return true;
+            else
+                return false;
+        }
+
+        void RevelarCarta(bool check)
+        {
+            Thread.Sleep(500);
+
+            foreach (PictureBox imagem in Controls.OfType<PictureBox>())
+            {
+                if (int.Parse(String.Format("{0}", imagem.Tag)) == guardarTag[0] ||
+                    int.Parse(String.Format("{0}", imagem.Tag)) == guardarTag[1])
+                {
+
+                    if (check == true)
+                    {
+                        imagem.Enabled = false;
+                        cartasEncontradas++;
+                    }
+                    else
+                    {
+                        imagem.Image = Properties.Resources.narutoLogo;
+                        imagem.Refresh();
+                    }
+                }
+            }
+
+            Acabo();
+        }
+    
+   
+
+        void Acabo()
+        {
+            if (cartasEncontradas == img.Length * 2)
+            {
+                MessageBox.Show("Parabéns você venceu o jogo com " + mov.ToString(), " movimentos");
+                DialogResult msg = MessageBox.Show("Quer continuar jogando?", "Caixa de pergunta", MessageBoxButtons.YesNo);
+
+                if(msg == DialogResult.Yes)
+                {
+                    click = 0; mov = 0; cartasEncontradas = 0;
+                    lista.Clear();
+                    Começar();
+                }
+                else if(msg == DialogResult.No)
+                {
+                    MessageBox.Show("Obrigado pro jogar :)");
+                    Application.Exit();
+                }
+            }
+        }
+    }
+
+}
